7f9ebe7fcde09f0eaf49b3e1025b2682190a4e4e
RouxTronics <rouxtronics@gmail.com>
2026-05-24 13:21:43 +0200

    update

--- C/2_Pointers/pointer.c
+++ C/2_Pointers/pointer.c
@@ -1,2 +1,11 @@
 #include <stdio.h>
 
+int main(){
+    float x = 50;
+    float *ptr = &x;
+
+    printf("Value of x: %f\n", x);
+    printf("Address of x: %p\n", &x);
+    printf("Value of ptr: %p\n", ptr);
+    printf("Value pointed to by ptr: %f\n", *ptr);
+}
